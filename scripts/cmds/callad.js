dca5fb3ef15b946b46dc54e458c48efeeab6af9a
Pharouk34 <164754483+Pharouk34@users.noreply.github.com>
2024-04-08 14:33:17 +0000

    Update callad.js

--- scripts/cmds/callad.js
+++ scripts/cmds/callad.js
@@ -37,12 +37,12 @@ module.exports = {
 			missingMessage: "Please enter the message you want to send to admin",
 			sendByGroup: "\n- Sent from group: %1\n- Thread ID: %2",
 			sendByUser: "\n- Sent from user",
-			content: "\n\nContent:\n⇰⇰⇰⇰⇰⇰⇰⇰⇰⇰⇰⇰⇰⇰⇰⇰⇰⇰⇰⇰\n%1\n⇰⇰⇰⇰⇰⇰⇰⇰⇰⇰⇰⇰⇰⇰⇰⇰⇰⇰⇰⇰⇰⇰\n 𓅓𝗗𝗥𝗔𝗖𝗨𝗟𝗔𓅓",
+			content: "\n\nContent:————————————————————\n\n⟨%1⟩\n\n———————————————————— 𓅓𝗗𝗥𝗔𝗖𝗨𝗟𝗔𓅓",
 			success: "Sent your message to %1 admin successfully!\n%2",
 			failed: "An error occurred while sending your message to %1 admin\n%2\nCheck console for more details",
-			reply: "\n\n✈︎✈︎✈︎✈︎✈︎✈︎✈︎✈︎✈︎✈︎✈︎✈︎✈︎✈︎✈︎✈︎\n\n🎶%2🎶\n\n✈︎✈︎✈︎✈︎✈︎✈︎✈︎✈︎✈︎✈︎✈︎✈︎✈︎✈︎✈︎✈︎✈︎✈︎✈︎✈︎✈︎ 𓅓𝗗𝗥𝗔𝗖𝗨𝗟𝗔𓅓",
+			reply: "_________________________\n\n⟨%2⟩\n\n______________________ 𓅓𝗗𝗥𝗔𝗖𝗨𝗟𝗔𓅓",
 			replySuccess: "Sent your reply to admin successfully!",
-			feedback: "📝 Feedback from user %1:\n- User ID: %2%3\n\nContent:\n─────────────────\n%4\n─────────────────\nReply this message to send message to user",
+			feedback: "📝 Feedback from user %1:\n- User ID: %2%3\n\nContent:─────────────────\n\n⟨%4⟩\n\n─────────────────𓅓𝘿𝙍𝘼𝘾𝙐𝙇𝘼𓅓",
 			replyUserSuccess: "Sent your reply to user successfully!",
 			noAdmin: "Bot has no admin at the moment"
 		}
