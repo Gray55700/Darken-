bec21b48d769854af1cf5570a56d04667811e4f9
Pharouk34 <164754483+Pharouk34@users.noreply.github.com>
2024-04-08 14:12:50 +0000

    Update callad.js

--- scripts/cmds/callad.js
+++ scripts/cmds/callad.js
@@ -37,10 +37,10 @@ module.exports = {
 			missingMessage: "Please enter the message you want to send to admin",
 			sendByGroup: "\n- Sent from group: %1\n- Thread ID: %2",
 			sendByUser: "\n- Sent from user",
-			content: "\n\nContent:\n─────────────────\n%1\n─────────────────\nReply this message to send message to user",
+			content: "\n\nContent:\n⇰⇰⇰⇰⇰⇰⇰⇰⇰⇰⇰⇰⇰⇰⇰⇰⇰⇰⇰⇰\n%1\n⇰⇰⇰⇰⇰⇰⇰⇰⇰⇰⇰⇰⇰⇰⇰⇰⇰⇰⇰⇰⇰⇰\n 𓅓𝗗𝗥𝗔𝗖𝗨𝗟𝗔𓅓",
 			success: "Sent your message to %1 admin successfully!\n%2",
 			failed: "An error occurred while sending your message to %1 admin\n%2\nCheck console for more details",
-			reply: "📍 Reply from admin %1:\n─────────────────\n%2\n─────────────────\nReply this message to continue send message to admin",
+			reply: "\n\n✈︎✈︎✈︎✈︎✈︎✈︎✈︎✈︎✈︎✈︎✈︎✈︎✈︎✈︎✈︎✈︎\n\n🎶%2🎶\n\n✈︎✈︎✈︎✈︎✈︎✈︎✈︎✈︎✈︎✈︎✈︎✈︎✈︎✈︎✈︎✈︎✈︎✈︎✈︎✈︎✈︎ 𓅓𝗗𝗥𝗔𝗖𝗨𝗟𝗔𓅓",
 			replySuccess: "Sent your reply to admin successfully!",
 			feedback: "📝 Feedback from user %1:\n- User ID: %2%3\n\nContent:\n─────────────────\n%4\n─────────────────\nReply this message to send message to user",
 			replyUserSuccess: "Sent your reply to user successfully!",
@@ -56,7 +56,7 @@ module.exports = {
 		if (config.adminBot.length == 0)
 			return message.reply(getLang("noAdmin"));
 		const senderName = await usersData.getName(senderID);
-		const msg = "==📨️ CALL ADMIN 📨️=="
+		const msg = "==𓅓 𝗖𝗔𝗟𝗟 𝗗𝗥𝗔𝗖𝗨𝗟𝗔 𓅓=="
 			+ `\n- User Name: ${senderName}`
 			+ `\n- User ID: ${senderID}`
 			+ (isGroup ? getLang("sendByGroup", (await threadsData.get(threadID)).threadName, threadID) : getLang("sendByUser"));
